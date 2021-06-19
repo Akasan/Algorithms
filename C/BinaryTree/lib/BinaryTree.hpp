0966ba04f91427ab08f0850a76ab210edf2e0dc1
赤川大空 <daisuke1024akagawa@gmail.com>
2021-06-19 23:59:27 +0900

    Fix indent

--- C/BinaryTree/lib/BinaryTree.hpp
+++ C/BinaryTree/lib/BinaryTree.hpp
@@ -4,13 +4,13 @@
 
 struct Node{
 public:
-   	struct Node* parent;
-   	struct Node* left;
-   	struct Node* right;
-   	int value;
-   	Node(int);
-   	void setLeft(Node*);
-   	void setRight(Node*);
+    struct Node* parent;
+    struct Node* left;
+    struct Node* right;
+    int value;
+    Node(int);
+    void setLeft(Node*);
+    void setRight(Node*);
 };
 
 
