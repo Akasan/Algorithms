997e1cb9c5ab4b9a0909c1c14cf6c8df0d7d759b
赤川大空 <daisuke1024akagawa@gmail.com>
2021-06-19 23:13:13 +0900

    Fix indent

--- C/BinaryTree/Main.cpp
+++ C/BinaryTree/Main.cpp
@@ -12,39 +12,39 @@
 
 int printNode(int cnt, Node node, int depth)
 {
-		for(int i=0; i<cnt; i++){
-				std::cout << " ";
-		}
-		std::cout << node.value << std::endl;
-		for(int i=0; i<cnt; i++){
-				std::cout << " ";
-		}
-		std::cout << "LEFT" << std::endl;
-		while(node.left != NULL){
-				printNode(cnt+1, *node.left, depth);
-				break;
-		}
-		for(int i=0; i<cnt; i++){
-				std::cout << " ";
-		}
-    	std::cout << "RIGHT" << std::endl;
-		while(node.right != NULL){
-				printNode(cnt+1, *node.right, depth);
-				break;
-		}
-		return depth +1;
+	for(int i=0; i<cnt; i++){
+			std::cout << " ";
+	}
+	std::cout << node.value << std::endl;
+	for(int i=0; i<cnt; i++){
+			std::cout << " ";
+	}
+	std::cout << "LEFT" << std::endl;
+	while(node.left != NULL){
+			printNode(cnt+1, *node.left, depth);
+			break;
+	}
+	for(int i=0; i<cnt; i++){
+			std::cout << " ";
+	}
+	std::cout << "RIGHT" << std::endl;
+	while(node.right != NULL){
+			printNode(cnt+1, *node.right, depth);
+			break;
+	}
+	return depth +1;
 }
 
 int main(void){
-		std::vector<int> array(SIZE);
-		for(int i=0; i<array.size(); i++)array[i] = i+1;
-		std::random_device seed_gen;
-		std::mt19937 engine(seed_gen());
-		std::shuffle(array.begin(), array.end(), engine);
+	std::vector<int> array(SIZE);
+	for(int i=0; i<array.size(); i++)array[i] = i+1;
+	std::random_device seed_gen;
+	std::mt19937 engine(seed_gen());
+	std::shuffle(array.begin(), array.end(), engine);
 
-		Node root = make_graph_simple(array);
-		std::cout << root.left << std::endl;
+	Node root = make_graph_simple(array);
+	std::cout << root.left << std::endl;
 
-		int depth = printNode(0, root, 1);
-		std::cout << depth << std::endl;
+	int depth = printNode(0, root, 1);
+	std::cout << depth << std::endl;
 }
