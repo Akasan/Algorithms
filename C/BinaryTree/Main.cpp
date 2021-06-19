2ee98e1c62e949789ab3c54f4a4bc89e6fcdfe01
赤川大空 <daisuke1024akagawa@gmail.com>
2021-06-19 23:15:32 +0900

    Fix indent

--- C/BinaryTree/Main.cpp
+++ C/BinaryTree/Main.cpp
@@ -13,24 +13,24 @@
 int printNode(int cnt, Node node, int depth)
 {
 	for(int i=0; i<cnt; i++){
-			std::cout << " ";
+		std::cout << " ";
 	}
 	std::cout << node.value << std::endl;
 	for(int i=0; i<cnt; i++){
-			std::cout << " ";
+		std::cout << " ";
 	}
 	std::cout << "LEFT" << std::endl;
 	while(node.left != NULL){
-			printNode(cnt+1, *node.left, depth);
-			break;
+		printNode(cnt+1, *node.left, depth);
+		break;
 	}
 	for(int i=0; i<cnt; i++){
-			std::cout << " ";
+		std::cout << " ";
 	}
 	std::cout << "RIGHT" << std::endl;
 	while(node.right != NULL){
-			printNode(cnt+1, *node.right, depth);
-			break;
+		printNode(cnt+1, *node.right, depth);
+		break;
 	}
 	return depth +1;
 }
